1d4652bf9249ac9818606c31ef581325c096188a
Felipe <lipebigol@gmail.com>
2021-06-14 21:02:58 -0300

    removido conteudo da home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,4 @@
-import { getPrismicClient } from "../../prismic"
-import Prismic from '@prismicio/client'
-import Header from '../components/Header'
 
-import { Flex, Heading, Text } from '@chakra-ui/react'
-
-import { RichText } from "prismic-dom"
 
 export default function Home({ advs }) {
   return (
@@ -13,28 +7,4 @@ export default function Home({ advs }) {
     </div>
 
   )
-}
-
-export const getStaticProps = async () => {
-  const prismic = getPrismicClient();
-
-  const response = await prismic.query(
-    [Prismic.Predicates.at('document.type', 'adv')]
-  )
-
-  const advs = response.results.map(adv => {
-    return {
-      slug: adv.uid,
-      title: RichText.asText(adv.data.title),
-      description: RichText.asText(adv.data.description),
-      body: RichText.asHtml(adv.data.body),
-    }
-  })
-
-  return {
-    props: {
-      advs,
-    },
-    revalidate: 1800,
-  }
 }
